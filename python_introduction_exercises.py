4979f1b69dd4fce42f30718fdb6092b35428113a
Luke Becker <luke.w.becker32@gmail.com>
2020-07-29 08:48:48 -0500

    adding introduction exercises to repo

---
+++ python_introduction_exercises.py
@@ -0,0 +1,12 @@
+print('Hello, World!')
+
+greeting = 'Hello, welcome to python'
+
+my_name = input('What is your name? ')
+
+greeting_name = greeting + " " + my_name + "!"
+
+print(greeting_name)
+
+
+
