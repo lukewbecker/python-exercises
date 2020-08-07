bbd8db05f2b429a70dc347b052c7ad337a361559
Luke Becker <luke.w.becker32@gmail.com>
2020-08-07 09:12:15 -0500

    added python exercise files to remote repo

--- function_exercises.py
+++ function_exercises.py
@@ -334,4 +334,12 @@ twentyfourto12("13:45")
 #     - col_index('B') returns 2
 #     - col_index('AA') returns 27
 
+def col_index(col_name):
+    alphabet_dict = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N":14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26}
+    column_num = 0
+    for index in range(-1, -(len(col_name)+1), -1):
+        column_num += alphabet_dict[(col_name[index])] * (26 ** -(index + 1))
+    return column_num
+col_index("AAA")
 
+# Thanks to Adam for his walkthrough - he cracked the code on how to count the columns together. Assuming this one won't count, but wanted to re-type at least to get the practice.
